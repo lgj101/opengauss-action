a091af174ac5fc0103a8f24802823185aa875596
ligjn <lgj112113@163.com>
2023-07-31 15:12:56 +0800

    edit env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 docker_run="docker run"
-docker_run="$docker_run -e GS_USERNAME=$INPUT_GS_USERNAME"
-docker_run="$docker_run -e GS_PASSWORD=$INPUT_GS_PASSWORD"
-docker_run="$docker_run -d -p 5432:5432 enmotech/opengauss:$INPUT_GS_VERSION"
+docker_run="$docker_run -e GS_USERNAME=$GS_USERNAME"
+docker_run="$docker_run -e GS_PASSWORD=$GS_PASSWORD"
+docker_run="$docker_run -d -p 5432:5432 enmotech/opengauss:$GS_VERSION"
 
 sh -c "$docker_run"
