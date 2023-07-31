e83827aca9f20c4eff08da22e11d5f6e88c3c8f3
namo <1733948498@qq.com>
2023-07-31 14:21:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 docker_run="docker run"
-docker_run="$docker_run -e POSTGRES_DB=$INPUT_POSTGRESQL_DB"
-docker_run="$docker_run -e POSTGRES_USER=$INPUT_POSTGRESQL_USER"
-docker_run="$docker_run -e POSTGRES_PASSWORD=$INPUT_POSTGRESQL_PASSWORD"
-docker_run="$docker_run -d -p 5432:5432 postgres:$INPUT_POSTGRESQL_VERSION"
+docker_run="$docker_run -e GS_PASSWORD=$INPUT_GS_PASSWORD"
+docker_run="$docker_run -d -p 5432:5432 enmotech/opengauss:$INPUT_GS_VERSION"
 
 sh -c "$docker_run"
