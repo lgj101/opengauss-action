b05430cc72ad3684652b79c982a63ef512c98c78
ligjn <lgj112113@163.com>
2023-08-01 10:38:52 +0800

    edit cmd

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ docker_run="docker run"
 docker_run="$docker_run -e GS_USERNAME=$INPUT_GS_USERNAME"
 docker_run="$docker_run -e GS_PASSWORD=$INPUT_GS_PASSWORD"
 docker_run="$docker_run -e GS_DB=$INPUT_GS_DB"
-docker_run="$docker_run -d -p $INPUT_HOST_PORT:$INPUT_CONTAINER_PORT enmotech/opengauss:$INPUT_GS_VERSION --port=$INPUT_CONTAINER_PORT"
+docker_run="$docker_run --privileged=true -d -p $INPUT_HOST_PORT:$INPUT_CONTAINER_PORT enmotech/opengauss:$INPUT_GS_VERSION --port=$INPUT_CONTAINER_PORT"
 
 sh -c "$docker_run"
